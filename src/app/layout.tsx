d1a59ada0c01148688bd5ab2d66776dea129e651
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-11-23 23:22:35 -0400

    :construction:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} `}>
-        {/* <Header /> */}
+        <Header />
         {children}
       </body>
     </html>
