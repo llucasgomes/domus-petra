90f9354ab3deb1c369c9477bf99c1ecb8a009547
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2024-07-13 10:09:50 -0400

    chore: moved for folder provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { ThemeProvider } from './_components/theme-provider'
+
+import { ThemeProvider } from './_provider/theme-provider'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
