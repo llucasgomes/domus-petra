af89bc30c5e7fdc5f2462af2d8ddcc3ed253b3dc
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2024-07-14 23:25:24 -0400

    :lock: feat: add/setup NextAuthjs + Google

--- src/components/header/auth.tsx
+++ src/components/header/auth.tsx
@@ -4,10 +4,7 @@ import { signIn, signOut, useSession } from 'next-auth/react'
 import { Button } from '../ui/button'
 
 export const LogOut = () => {
-  const { data } = useSession()
-  return (
-    <>{data && <Button onClick={async () => await signOut()}>Sair</Button>}</>
-  )
+  return <Button onClick={async () => await signOut()}>Sair</Button>
 }
 
 export const LogIn = () => {
