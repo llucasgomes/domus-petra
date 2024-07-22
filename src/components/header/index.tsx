83e93eb6ee4f4989d8fc80eee2708e8c7833e818
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2024-07-22 09:25:44 -0400

    :lipstick: styles(component/header):logica de troca de bg, entre rotas

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,14 @@
 'use client'
 
 import Image from 'next/image'
+import { usePathname } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import { MenuMobile } from './menuMobile'
 import { NavHeader } from './nav'
 
 export const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false)
+  const pathname = usePathname()
 
   useEffect(() => {
     const handleScroll = () => {
@@ -21,7 +23,7 @@ export const Header = () => {
 
   return (
     <header
-      className={`fixed top-0 z-10 flex h-20 w-full items-center justify-between gap-6 px-8 transition-colors duration-300 ${isScrolled ? 'bg-[--bg-highlight]' : 'bg-transparent'}`}
+      className={`fixed top-0 z-10 flex h-20 w-full items-center justify-between gap-6 px-8 transition-colors duration-300 ${pathname == '/' ? (isScrolled ? 'bg-[--bg-highlight]' : 'bg-transparent') : 'bg-[--bg-highlight]'}`}
     >
       <Image
         src={'/logoHorizontalWhite.png'}
