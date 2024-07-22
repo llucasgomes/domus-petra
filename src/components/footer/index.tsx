ff23a26890117f0622b56acaf5642e3a20aa8ad8
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2024-07-22 06:56:43 -0400

    :lipstick: chore(component/footer):pequena estilizacao

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,62 +1,52 @@
-/* eslint-disable prettier/prettier */
-import Image from 'next/image'
-import { FaInstagram, FaLinkedin, FaWhatsapp, FaYoutube } from 'react-icons/fa'
+import { FacebookIcon } from 'lucide-react'
+import Link from 'next/link'
+import { FaInstagram, FaLinkedin, FaYoutube } from 'react-icons/fa'
 import styles from './Footer.module.css'
+
+export const redes_sociais = [
+  {
+    rede_social: 'Linkedin',
+    icon: <FaLinkedin />,
+    link: 'https://www.linkedin.com/in/fabianodellagnolo/',
+  },
+  {
+    rede_social: 'Youtube',
+    icon: <FaYoutube />,
+    link: 'https://www.youtube.com/FabianoDellAgnoloDomusPetra',
+  },
+  {
+    rede_social: 'Instagram',
+    icon: <FaInstagram />,
+    link: 'https://www.instagram.com/fabianodellagnolo/',
+  },
+  {
+    rede_social: 'whatszap',
+    icon: <FacebookIcon />,
+    link: 'https://api.whatsapp.com/send/?phone=47997071177',
+  },
+]
 
 export const Footer = () => {
   return (
-    <footer className={styles.footer}>
-      <div className={styles.footerContainer}>
-        <Image
-          src={'/logoOpenGraph.png'}
-          alt="Logo"
-          width={640}
-          height={128}
-          className={styles.logo}
-          // eslint-disable-next-line prettier/prettier
-        />
-        <div className={styles.socialMedia}>
-          <a
-            href="https://www.linkedin.com/in/fabianodellagnolo/"
-            target="_blank"
-            title="Linkedin"
-            rel="noreferrer"
-            className={styles.socialLink}
-          >
-            <FaLinkedin />
-          </a>
-          <a
-            href="https://www.youtube.com/FabianoDellAgnoloDomusPetra
-"
-            target="_blank"
-            title="YouTube"
-            rel="noreferrer"
-            className={styles.socialLink}
-          >
-            <FaYoutube />
-          </a>
-          <a
-            href="https://www.instagram.com/fabianodellagnolo/"
-            target="_blank"
-            title="Instagram"
-            rel="noreferrer"
-            className={styles.socialLink}
-          >
-            <FaInstagram />
-          </a>
-          <a
-            href="https://api.whatsapp.com/send/?phone=47997071177"
-            target="_blank"
-            rel="external noreferrer"
-            title="Whatsapp"
-            className={styles.socialLink}
-          >
-            <FaWhatsapp />
-          </a>
+    <footer className="flex flex-col gap-3 bg-[--bg-highlight] p-5">
+      <div className="flex items-center justify-between gap-3">
+        <div className="flex w-full items-center justify-center gap-5">
+          {redes_sociais.map((item) => (
+            <Link
+              key={item.link}
+              href={item.link}
+              target="_blank"
+              className={styles.socialLink}
+            >
+              {item.icon}
+            </Link>
+          ))}
         </div>
       </div>
-      <p className={styles.copyright}>
-        © 2024 <span>Fabiano Dell Agnelo</span>. Todos os direitos reservados.
+      <p className="flex items-center justify-center">
+        © 2024{' '}
+        <span className="text-[--bg-tertiary]">Fabiano Dell Agnelo</span>. Todos
+        os direitos reservados.
       </p>
     </footer>
   )
