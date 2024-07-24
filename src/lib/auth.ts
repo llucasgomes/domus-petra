5c65850ed87ad31e33f1c7996e37682793176967
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2024-07-24 07:50:05 -0400

    :bug: prisma

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,11 @@
+import { PrismaAdapter } from '@auth/prisma-adapter'
 import { AuthOptions } from 'next-auth'
+import { Adapter } from 'next-auth/adapters'
 import GoogleProvider from 'next-auth/providers/google'
+import { db } from './prisma'
 
 export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(db) as Adapter,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
