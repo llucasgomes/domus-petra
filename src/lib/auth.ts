1eaef16b9459218a234281a0915a5d9ef1f1a99f
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2024-07-24 07:29:31 -0400

    :bug: fix:NextAuth

---
+++ src/lib/auth.ts
@@ -0,0 +1,23 @@
+import { AuthOptions } from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+
+export const authOptions: AuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+    })
+  ],
+  callbacks: {
+    async session({ session, user }) {
+      session.user = { ...session.user, id: user.id } as {
+        id: string
+        name: string
+        email: string
+      }
+
+      return session
+    }
+  },
+  secret: process.env.NEXT_AUTH_SECRET
+}
