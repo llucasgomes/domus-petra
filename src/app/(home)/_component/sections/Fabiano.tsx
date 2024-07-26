988d6b8953946f278c365545043e5b2a6f4db05c
RafaRz76Dev <rafarz76dev@gmail.com>
2024-07-26 12:46:14 -0300

    :lipstick: chore(page/home): restruturando o section Fabiano

--- src/app/(home)/_component/sections/Fabiano.tsx
+++ src/app/(home)/_component/sections/Fabiano.tsx
@@ -1,55 +1,55 @@
-import { poppis } from '@/app/_ui/fonts'
+/* eslint-disable prettier/prettier */
+import { poppis, montserrat } from '@/app/_ui/fonts'
 import Image from 'next/image'
 
 export const FabianoSobre = () => {
   return (
-    <section className="relative flex-col items-center gap-12 p-8 md:h-[500px] md:flex-row md:items-start md:justify-start md:gap-8 md:p-10 lg:h-screen">
-      <div className="flex h-full w-full flex-col justify-center gap-4 md:w-[600px] lg:justify-center lg:gap-2 lg:px-8">
-        <div className="">
+    <section className="relative flex h-screen flex-col items-center justify-center bg-gradient-to-b from-[#131727] via-[#2B355A] to-[#131727] p-8 md:flex-row md:items-center md:justify-center md:p-10">
+      <div className="flex flex-col items-center text-center md:mr-[100px] md:w-[600px] md:items-start md:gap-8 md:text-left">
+        <div className="mb-8 mt-16 md:mb-2 md:mt-4">
           <h2
-            className={`${poppis.className} text-3xl font-black text-white md:text-7xl lg:text-9xl xl:text-8xl`}
+            className={`${poppis.className} text-[180px] font-black leading-none text-white md:text-[150px]`}
           >
             FABIANO
           </h2>
           <h2
-            className={`${poppis.className} text-3xl font-black text-[#567CAB] md:text-7xl lg:text-9xl xl:text-8xl`}
+            className={`${poppis.className} text-[180px] font-black leading-none text-[#567CAB] md:text-[150px]`}
           >
             {`DELL'AGNOLO`}
           </h2>
         </div>
-        <Image
-          alt="homem de pele clara com cabelo curto e escuro, usando um terno cinza claro sobre uma camisa social azul clara. Ele está em pé, falando com um microfone de fone de ouvido preso à orelha e um controle remoto nas mãos. A expressão dele é séria e ele parece estar apresentando ou palestrando em um evento. O fundo é escuro, destacando a figura do homem."
-          src={'/outros/profile-fabiano.png'}
-          width={380}
-          height={380}
-          className="md:hidden"
-        />
-        <p className="text-white md:w-[500px] lg:w-[672px] lg:text-lg">
+        <p
+          className={`${montserrat.className} text-[22px] text-white md:mt-8 md:w-[600px] md:text-[18px]`}
+        >
           Com mais de{' '}
           <span className="font-semibold">20 anos de experiência</span> na
           indústria, focado em metal-mecânica e automotivo, certificado em Seis
           Sigma e ISO, e especializado em
           <span className="font-semibold">Melhoria Contínua</span> e{' '}
-          <span className="font-semibold">Lean Manufacturing</span>. Atua como
-          consultor em gestão empresarial, promovendo{' '}
+          <span className="font-semibold">Lean Manufacturing</span>.
+          <br /> <br />
+          Atua como consultor em gestão empresarial, promovendo{' '}
           <span className="font-semibold">eficiência</span> e{' '}
           <span className="font-semibold">implementação de metodologias</span>.
           Diretor executivo na Secretaria de Planejamento Urbano de Joinville,
           liderando projetos de desenvolvimento sustentável e tecnológico.
+          <br />
+          <br />
           Engajado em iniciativas de{' '}
           <span className="font-semibold">empreendedorismo e inovação</span>,
           como presidente do COMCITI e colunista no portal NSC Tech.
         </p>
       </div>
 
-      <Image
-        alt="homem de pele clara com cabelo curto e escuro, usando um terno cinza claro sobre uma camisa social azul clara. Ele está em pé, falando com um microfone de fone de ouvido preso à orelha e um controle remoto nas mãos. A expressão dele é séria e ele parece estar apresentando ou palestrando em um evento. O fundo é escuro, destacando a figura do homem."
-        src={'/outros/profile-fabiano02.png'}
-        width={580}
-        height={580}
-        className="absolute bottom-0 right-2 -z-10 hidden brightness-50 md:right-0 md:flex md:w-96 md:brightness-75 lg:w-[700px] xl:w-[550px]"
-      />
-      <div className="absolute bottom-0 left-0 -z-20 h-full w-full bg-gradient-to-b from-[#131727] from-0% via-[#2B355A] via-50% to-[#131727] to-100%"></div>
+      <div className="relative w-full md:ml-10 md:w-[750px]">
+        <Image
+          alt="homem de pele clara com cabelo curto e escuro, usando um terno cinza claro sobre uma camisa social azul clara. Ele está em pé, falando com um microfone de fone de ouvido preso à orelha e um controle remoto nas mãos. A expressão dele é séria e ele parece estar apresentando ou palestrando em um evento. O fundo é escuro, destacando a figura do homem."
+          src={'/outros/profile-fabiano02.png'}
+          width={1050}
+          height={1134}
+          className="w-full max-w-[1050px] md:mt-12" // Ajusta a margem superior da imagem
+        />
+      </div>
     </section>
   )
 }
