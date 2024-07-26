9f6964c763bfeb898374df60253ad06c31eda8a3
RafaRz76Dev <rafarz76dev@gmail.com>
2024-07-25 23:19:32 -0300

    :wrench: chore: :sparkles: feat(components/Footer) and (public/logo) adicionado uma nova imagem logo

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,49 +1,63 @@
-import { FacebookIcon } from 'lucide-react'
+/* eslint-disable prettier/prettier */
+import Image from 'next/image'
 import Link from 'next/link'
-import { FaInstagram, FaLinkedin, FaYoutube } from 'react-icons/fa'
-import styles from './Footer.module.css'
+import { FaEnvelope, FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa'
+import styles from './Footer.module.css' // Importando o CSS
 
 export const redes_sociais = [
-  {
-    rede_social: 'Linkedin',
-    icon: <FaLinkedin />,
-    link: 'https://www.linkedin.com/in/fabianodellagnolo/'
-  },
-  {
-    rede_social: 'Youtube',
-    icon: <FaYoutube />,
-    link: 'https://www.youtube.com/FabianoDellAgnoloDomusPetra'
-  },
   {
     rede_social: 'Instagram',
     icon: <FaInstagram />,
     link: 'https://www.instagram.com/fabianodellagnolo/'
   },
   {
-    rede_social: 'whatszap',
-    icon: <FacebookIcon />,
-    link: 'https://api.whatsapp.com/send/?phone=47997071177'
+    rede_social: 'Facebook',
+    icon: <FaFacebook />,
+    link: 'https://m.facebook.com/fabiano.dellagnolo/'
+  },
+  {
+    rede_social: 'email',
+    icon: <FaEnvelope />,
+    link: 'mailto:contato@domuspetra.com.br'
+  },
+  {
+    rede_social: 'Youtube',
+    icon: <FaYoutube />,
+    link: 'https://www.youtube.com/FabianoDellAgnoloDomusPetra'
   }
 ]
 
 export const Footer = () => {
   return (
-    <footer className="flex flex-col gap-3 bg-[--bg-highlight] p-5">
-      <div className="flex items-center justify-between gap-3">
-        <div className="flex w-full items-center justify-center gap-5">
-          {redes_sociais.map((item) => (
-            <Link
-              key={item.link}
-              href={item.link}
-              target="_blank"
-              className={styles.socialLink}
-            >
-              {item.icon}
-            </Link>
-          ))}
+    <footer className={styles.footer}>
+      <div className={styles.footerContainer}>
+        <div className={styles.footerLeft}>
+          <Image
+            src={'/logo/logoDomus-Footer.png'}
+            alt="Logo Domus Petra"
+            width={450}
+            height={95}
+          />
+        </div>
+        <div className={styles.footerRight}>
+          <p className={styles.footerText}>contato@domuspetra.com.br</p>
+          <p className={styles.footerText}>47 9 9707 1177</p>
+          <p className={styles.footerText}>Rua Tangará, 470 | Joinville</p>
+          <div className={styles.footerSocials}>
+            {redes_sociais.map((item) => (
+              <Link
+                key={item.link}
+                href={item.link}
+                target="_blank"
+                className="text-4xl text-primary hover:text-secondary"
+              >
+                {item.icon}
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
-      <p className="flex items-center justify-center">
+      <p className={`${styles.footerCopyright} text-sm`}>
         © 2024{' '}
         <span className="text-[--bg-tertiary]">Fabiano Dell Agnelo</span>. Todos
         os direitos reservados.
