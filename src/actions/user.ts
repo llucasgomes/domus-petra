965c6e832a6c328b8db5eb55699bd01f87fa320f
RafaRz76Dev <rafarz76dev@gmail.com>
2024-08-03 20:14:52 -0300

    :lipstick: chore (page/actions): Atualizando service and user

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -1,27 +1,27 @@
-'use server'
+// 'use server'
 
-import { db } from '@/lib/prisma'
+// import { db } from '@/lib/prisma'
 
-export async function GETUsers() {
-  const users = await db.user.findMany()
-  return users
-}
-export async function UPDATEUser(userId: string, newRole: string) {
-  try {
-    await db.user.update({
-      where: { id: userId },
-      data: { role: newRole }
-    })
-  } catch (error) {
-    console.log(error)
-  }
-}
+// export async function GETUsers() {
+//   const users = await db.user.findMany()
+//   return users
+// }
+// export async function UPDATEUser(userId: string, newRole: string) {
+//   try {
+//     await db.user.update({
+//       where: { id: userId },
+//       data: { role: newRole }
+//     })
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
 
-export async function GETUserSingle(userEmail: string) {
-  const user = await db.user.findUnique({
-    where: {
-      email: userEmail
-    }
-  })
-  return user
-}
+// export async function GETUserSingle(userEmail: string) {
+//   const user = await db.user.findUnique({
+//     where: {
+//       email: userEmail
+//     }
+//   })
+//   return user
+// }
